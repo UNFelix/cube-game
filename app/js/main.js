9a6708dcb875d5c58c939ef80c8dde15b66b0eac
unfelix <shevko.evheniy@gmail.com>
2020-11-30 12:10:59 +0200

    form style and some another

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
-$(function(){
+const openModalBtn = document.getElementById('button')
+const closeBtn = document.getElementById('close')
+const submitBtn = document.getElementById('submit')
+const modalDialog = document.getElementById('modal')
+const modalForm = document.forms[0]
 
 
-
-  
-});
+openModalBtn.onclick = () => modalDialog.showModal()
+closeBtn.onclick = () => modalDialog.close()
+submitBtn.onclick = () => modalForm.classList.add('checked')
